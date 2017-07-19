decd2f54b372c3425be18d6e8d6cbe3b3283f0b8
Josh <josh@eastfive.com>
2017-07-19 08:51:11 -0500

    added SML config settings

--- AppSettings.cs
+++ AppSettings.cs
@@ -15,5 +15,16 @@ namespace EastFive.Security
 
         public const string CredentialProviderVoucherKey = "EastFive.Security.CredentialProvider.Voucher.Key";
         public const string CredentialProviderVoucherProviderId = "EastFive.Security.CredentialProvider.Voucher.Provider";
+
+        /// <summary>
+        /// The certificate the SAML provider offers. It is in base64 format. Only the public key is availble. It is used
+        /// to verfiy the signature of the SAML assurtion.
+        /// </summary>
+        public const string SAMLCertificate = "EastFive.Security.CredentialProvider.SAML.Certificate";
+        /// <summary>
+        /// The name of the attribute in the SAML assurtion whos value contains the key that is used to lookup the
+        /// user in the local system.
+        /// </summary>
+        public const string SAMLLoginIdAttributeName = "EastFive.Security.CredentialProvider.SAML.LoginIdAttributeName";
     }
 }
