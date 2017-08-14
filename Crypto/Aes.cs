0d23de4c08cddf898442f9cd5a45f808ea0eaecc
Keith Holloway <keith@eastfive.com>
2017-08-14 16:08:18 -0500

    Added AesCrypto support

---
+++ Crypto/Aes.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.Security.Crypto
+{
+    public static class AesCrypto
+    {
+        public static byte[] AESEncrypt(this byte[] rawPlaintext, string password)
+        {
+            try
+            {
+                int Rfc2898KeygenIterations = 100;
+                int AesKeySizeInBits = 128;
+                var salt = Enumerable.Range(0, 16).Select(i => (byte)i).ToArray();
+                byte[] cipherText = null;
+                using (Aes aes = new AesManaged())
+                {
+                    aes.Padding = PaddingMode.PKCS7;
+                    aes.KeySize = AesKeySizeInBits;
+                    int KeyStrengthInBytes = aes.KeySize / 8;
+                    System.Security.Cryptography.Rfc2898DeriveBytes rfc2898 =
+                        new System.Security.Cryptography.Rfc2898DeriveBytes(password, salt, Rfc2898KeygenIterations);
+                    aes.Key = rfc2898.GetBytes(KeyStrengthInBytes);
+                    aes.IV = rfc2898.GetBytes(KeyStrengthInBytes);
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                        {
+                            cs.Write(rawPlaintext, 0, rawPlaintext.Length);
+                        }
+                        cipherText = ms.ToArray();
+                    }
+                    return cipherText;
+                }
+            }
+            //Catch and display a CryptographicException  
+            //to the console.
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+
+                return null;
+            }
+        }
+
+        public static byte[] AESDecrypt(this byte[] cipherText, string password)
+        {
+            try
+            {
+                int Rfc2898KeygenIterations = 100;
+                int AesKeySizeInBits = 128;
+                var salt = Enumerable.Range(0, 16).Select(i => (byte)i).ToArray();
+                using (Aes aes = new AesManaged())
+                {
+                    aes.Padding = PaddingMode.PKCS7;
+                    aes.KeySize = AesKeySizeInBits;
+                    int KeyStrengthInBytes = aes.KeySize / 8;
+                    System.Security.Cryptography.Rfc2898DeriveBytes rfc2898 =
+                        new System.Security.Cryptography.Rfc2898DeriveBytes(password, salt, Rfc2898KeygenIterations);
+                    aes.Key = rfc2898.GetBytes(KeyStrengthInBytes);
+                    aes.IV = rfc2898.GetBytes(KeyStrengthInBytes);
+                  
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                        {
+                            cs.Write(cipherText, 0, cipherText.Length);
+                        }
+                        var plainText = ms.ToArray();
+                        return plainText;
+                    }
+                }
+            }
+            //Catch and display a CryptographicException  
+            //to the console.
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+
+                return null;
+            }
+
+        }
+    }
+}
