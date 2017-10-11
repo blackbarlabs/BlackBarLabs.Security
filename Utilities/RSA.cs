2d54e6cf25f0b29b059a196baa32ca94bee3d3fa
Keith Holloway <keith@eastfive.com>
2017-10-11 15:57:36 -0500

    Added ability to get RSA by providing private key

--- Utilities/RSA.cs
+++ Utilities/RSA.cs
@@ -12,6 +12,30 @@ namespace BlackBarLabs.Security
 {
     public static class RSA
     {
+        public static TResult FromConfig<TResult>(string secretAsRSAXmlBase64,
+            Func<RSACryptoServiceProvider, TResult> success,
+            Func<string, TResult> invalidSecret)
+        {
+            try
+            {
+                var bytes = Convert.FromBase64String(secretAsRSAXmlBase64);
+                var xml = Encoding.ASCII.GetString(bytes);
+                var rsaProvider = new RSACryptoServiceProvider();
+                try
+                {
+                    rsaProvider.FromXmlString(xml);
+                    return success(rsaProvider);
+                }
+                catch (CryptographicException ex)
+                {
+                    return invalidSecret(ex.Message);
+                }
+            } catch(FormatException ex)
+            {
+                return invalidSecret(ex.Message);
+            }
+        }
+
         public static TResult FromConfig<TResult>(string configSettingName,
             Func<RSACryptoServiceProvider, TResult> success,
             Func<string, TResult> missingConfigurationSetting,
@@ -35,7 +59,8 @@ namespace BlackBarLabs.Security
                 {
                     return invalidConfigurationSetting(configSettingName, ex.Message);
                 }
-            } catch(FormatException ex)
+            }
+            catch (FormatException ex)
             {
                 return invalidConfigurationSetting(configSettingName, ex.Message);
             }
